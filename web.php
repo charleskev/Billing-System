a0c41bfb4bf3b342829c27df72578c6ac65cc6c2
charleskev <charleskevsevilla@gmail.com>
2025-09-01 20:14:16 +0800

    Update web.php

--- web.php
+++ web.php
@@ -1,1 +1,9 @@
+Route::get('/bill', [BillController::class, 'index']);
 
+
+Route::get('/', function () {
+    $customer_name = request('customer_name');
+    $customer_type = request('customer_type');
+    $consumption_kwh = request('consumption_kwh');
+    return view('bill', compact('customer_name', 'customer_type', 'consumption_kwh'));
+});
