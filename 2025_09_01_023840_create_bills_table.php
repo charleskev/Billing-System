a6e97ddd717d920dce39f63d6da8f8aed60d4cbd
charleskev <charleskevsevilla@gmail.com>
2025-09-01 20:16:16 +0800

    Update 2025_09_01_023840_create_bills_table.php

--- 2025_09_01_023840_create_bills_table.php
+++ 2025_09_01_023840_create_bills_table.php
@@ -1,1 +1,32 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+             $table->string('customer_name');
+                        $table->string('customer_type');
+                          $table->float('consumption_kwh');
+                        $table->float('base_bill');
+                                            $table->float('total_bill');
+
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
